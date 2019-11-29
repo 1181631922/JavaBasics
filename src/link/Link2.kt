02fdc233d29323ff9aea63584efd75aacc70d221
fanyafeng <fanyafeng@365rili.com>
2019-11-29 17:11:48 +0800

    添加第二个问题

---
+++ src/link/Link2.kt
@@ -0,0 +1,60 @@
+package link
+
+import link.Link1.insertNode
+
+/**
+ * Author： fanyafeng
+ * Data： 2019-11-29 16:29
+ * Email: fanyafeng@live.cn
+ */
+object Link2 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        //查找单链表倒数第k个元素
+        val temp = Link1.Node(1, null)
+        val temp2 = Link1.Node(2, null)
+        val temp3 = Link1.Node(3, null)
+        val temp4 = Link1.Node(4, null)
+        val temp5 = Link1.Node(5, null)
+        val temp6 = Link1.Node(6, null)
+
+        insertNode(temp, temp2)
+        insertNode(temp, temp3)
+        insertNode(temp, temp4)
+        insertNode(temp, temp5)
+        insertNode(temp, temp6)
+
+//        println(temp)
+//        println(findSomeNode(temp, 2))
+        printNode(temp)
+    }
+
+    fun <T> printNode(node: Link1.Node<T>) {
+        var temp = node
+        println(temp.data)
+        while (temp.next != null) {
+            temp = temp.next!!
+            println(temp.data)
+        }
+
+    }
+
+
+    //假设求倒数第2个元素
+    fun <T> findSomeNode(node: Link1.Node<T>, count: Int): Link1.Node<T> {
+        var slowNode = node
+        var fastNode = node
+        (0 until count).forEachIndexed { index, i ->
+            fastNode = fastNode.next!!
+        }
+
+        while (true) {
+            slowNode = slowNode.next!!
+            if (fastNode.next == null) {
+                return slowNode
+            } else {
+                fastNode = fastNode.next!!
+            }
+        }
+    }
+}
